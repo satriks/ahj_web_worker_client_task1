1a5b4472ba1b1fcc625be5c536d0133d0864dd02
Yury Vereskun <satriks@mail.ru>
2023-11-20 14:56:17 +0300

    update fetch 2

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -12,7 +12,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        return response || fetch(event.request, { credentials: 'include' })
+        return response || fetch(event.request)
       })
   )
 })
