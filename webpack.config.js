d74a2f075b25e89bbed9d876304b9eb00e0b7b86
Yury Vereskun <satriks@mail.ru>
2023-11-27 20:47:16 +0300

    change cach

--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,12 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } },
-      { test: /\.html$/, exclude: path.resolve(__dirname, 'src/js/service-worker.js'), use: [{ loader: 'html-loader' }] },
+      { test: /\.html$/, use: [{ loader: 'html-loader' }] },
       { test: /\.txt$/, use: 'raw-loader' },
       { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
       {
         test: /\.(png|jpg|gif|cur)$/i, dependency: { not: ['url'] }, use: [{ loader: 'url-loader', options: { limit: 8192 } }], type: 'javascript/auto',  // eslint-disable-line
       },
-      { test: /\.html$/, include: [path.resolve(__dirname, 'src/js/service-worker.js')], use: { loader: 'file-loader' } }
-      // { test: /sevice-worker\.js$/, use: { loader: "worker-loader" },},
     ]
   },
   resolve: {
