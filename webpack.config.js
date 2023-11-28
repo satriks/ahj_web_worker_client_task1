a786dab9f1d1d5c5a042f4bd4afbcc0aa57214a3
Yury Vereskun <satriks@mail.ru>
2023-11-28 16:21:32 +0300

    check process env 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const process = require('process')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
